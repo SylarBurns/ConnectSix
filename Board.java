0f31420f5c919cad10400ef7c2d9d88de7a04f25
SylarBurns <41365906+SylarBurns@users.noreply.github.com>
2018-09-13 12:00:06 +0900

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,18 @@
+package Connect6;
+
+
+public class Board {
+	public Coordinate[][] board = new Coordinate[19][19];	
+public void initiateBoard() {
+	  for(int i =0; i<19; i++) {
+		  for(int j=0; j<19; j++) {
+			  board[i][j]= new Coordinate(i,j,"BLK");
+			  board[i][j].printAll();
+		  }
+		  System.out.println();
+	  }
+}
+public void setStone(int x, int y, String S) {
+	board[x][y].setState(S);
+}
+}
