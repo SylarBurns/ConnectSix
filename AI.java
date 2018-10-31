719a3c0dad3917df0c5fddbbf183054f35f0d69d
sungminkim <ksm3169@gmail.com>
2018-10-31 21:54:10 +0900

    Edit the wrong implementation in evaluation clases

--- AI.java
+++ AI.java
@@ -1,45 +1,5 @@
 class AI{  // 컴퓨터가 계산해서 최적의 위치를 반환하는 class를 구현
     
-
-    int stoneColor ;
-    // Location [] board = new Location[361];
-
-
-
-
-    AI(int color){
-
-        this.stoneColor = color ;
-
-
-
-    }
-
-
-    Location where(){
-
-        return Gomoku.board[1];
-    }
-
-    void setBoard(){
-
-        for(int i=0;i<361;i++){
-
-            // board[i] = new Location();
-            // board[i] = Gomoku.board[i];
-            if(Gomoku.board[i].is != false){
-
-
-
-            }
-
-
-        }
-
-    }
-
-
-
-
-
+	
+	
 }
