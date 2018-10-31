31881ee411cdb912292caedc4ee8c3b745aef778
coolgogi <coolgogi@naver.com>
2018-10-31 23:04:01 +0900

    Goooooooooooood

--- Gomoku.java
+++ Gomoku.java
@@ -1,169 +1,174 @@
 import java.util.* ;
 import javax.swing.* ;
 import java.awt.*;
+import java.awt.event.KeyListener;
 
 public class Gomoku{
 
-	private static final int B = 0; 	 // color black : 0
-	private static final int W = 1; 	 // color white : 1
-	private static final int N = -1;	 // no stone    : -1
+   private static final int B = 0;     // color black : 0
+   private static final int W = 1;     // color white : 1
+   private static final int N = -1;    // no stone    : -1
  
-	public static Location [] board    = new Location[361]; //바둑판에 있는 돌
-	public static Location [] setboard = new Location[361]; //게임할 때 놓은 돌
+   public static Location [] board    = new Location[361]; //바둑판에 있는 돌
+   public static Location [] setboard = new Location[361]; //게임할 때 놓은 돌
 
-	public static boolean win = false ; //승패여부 저장
+   public static boolean win = false ; //승패여부 저장
 
-	public static int k = 1;   			// 유저가 놓은 돌 갯수 : 1인 이유는 검은색 돌 1개는 무조건 놓여있기 때문
+   public static int k = 1;            // 유저가 놓은 돌 갯수 : 1인 이유는 검은색 돌 1개는 무조건 놓여있기 때문
 
-	public static int turnW = 0;		//White turn
-	public static int turnB = 0;		//Black turn
-	public static Location User = new Location()  ; // Cursor를 위한 Location객체 만들어주기
-
-
-	public static int userColor = N;	//store user's color 
+   public static int turnW = 0;      //White turn
+   public static int turnB = 0;      //Black turn
+   public static Location User = new Location()  ; // Cursor를 위한 Location객체 만들어주기
 
 
-
-	public static void main(String[] args){
-
-		while(true){		//Select User's color not AI color
-			System.out.println("\nSelect Your color(B or W) : ");
-			Scanner t = new Scanner(System.in);
-
-			char stoneColor = t.next().charAt(0) ;
-			if(stoneColor == 'B')      
-			{
-				userColor = B ;
-				System.out.println("Black") ;
-				break;
-			}
-			else if(stoneColor == 'W')
-			{
-				userColor = W ;
-				System.out.println("White") ;
-				break;
-			}else{
-				System.out.println("Wrong Input. Enter Again!");
-			}
-		}
+   public static int userColor = N;   //store user's color 
 
 
 
-		JFrame frame = new JFrame("Gomoku") ;
-		Board GB = new Board() ;
-		frame.getContentPane().add(GB) ;
-		
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE) ;
-		frame.setSize(1100, 1100) ;
-		frame.setVisible(true) ;
-		
+   public static void main(String[] args){
+
+      while(true){      //Select User's color not AI color
+         System.out.println("\nSelect Your color(B or W) : ");
+         Scanner t = new Scanner(System.in);
+
+         char stoneColor = t.next().charAt(0) ;
+         if(stoneColor == 'B')      
+         {
+            userColor = B ;
+            System.out.println("Black") ;
+            break;
+         }
+         else if(stoneColor == 'W')
+         {
+            userColor = W ;
+            System.out.println("White") ;
+            break;
+         }else{
+            System.out.println("Wrong Input. Enter Again!");
+         }
+      }
 
 
-		int num  = 0 ;
-		int numx = 0 ;
-		int numy = 0 ;
-		int i,j ;
 
-		for(i=0; i<19; i++) //모든 Location에 X,Y값 부여
-			for(j=0; j<19; j++){
+      JFrame frame = new JFrame("Gomoku") ;
+      Board GB = new Board() ;
+      frame.getContentPane().add(GB) ;
+      
+      frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE) ;
+      frame.setSize(1100, 1100) ;
+      frame.setFocusable(true);
+      
+      int num  = 0 ;
+      int numx = 0 ;
+      int numy = 0 ;
+      int i,j ;
 
-				num = (i*19) + j  ;
-				numx = 10 +(40*j) ;
-				numy = 10 +(40*i) ;
+      for(i=0; i<19; i++) //모든 Location에 X,Y값 부여
+         for(j=0; j<19; j++){
 
-				board[num] = new Location() ;
-				board[num].SetLocation(numx, numy) ;
-				board[num].Setis(false) ;
+            num = (i*19) + j  ;
+            numx = 10 +(40*j) ;
+            numy = 10 +(40*i) ;
 
-				setboard[num] = new Location() ;
-				setboard[num].Setis(false) ;
-			}
-		
-		User.SetLocation(370, 370);
-		int keycode = 0 ;
-		ck c1 = new ck();
+            board[num] = new Location() ;
+            board[num].SetLocation(numx, numy) ;
+            board[num].Setis(false) ;
 
-		totalScore total_Score = new totalScore();
+            setboard[num] = new Location() ;
+            setboard[num].Setis(false) ;
+         }
+      
+      User.SetLocation(370, 370);
+      int keycode = 0 ;
+      ck c1 = new ck();
 
-		board[180].Setis(true) ; 		 // 검은색 돌은 처음에 가운데에 무조건 놓여있어야 한다. 
-		board[180].BW = B ; 
-		setboard[0] = board[180] ;
-		total_Score.setTotal(setboard[0]);
-		total_Score.display();
+      totalScore total_Score = new totalScore();
 
-		turnW = 2;  						// white has two turn in first
+      board[180].Setis(true) ;        // 검은색 돌은 처음에 가운데에 무조건 놓여있어야 한다. 
+      board[180].BW = B ; 
+      setboard[0] = board[180] ;
+      total_Score.setTotal(setboard[0]);
+      total_Score.display();
 
+      turnW = 2;                    // white has two turn in first
+      System.out.println("Check");
+         class Key implements KeyListener {
+            public void keyPressed(java.awt.event.KeyEvent e) {
+                  /* 커서 움직이기 하지만 개선이 되어야 한다!!! */
+                  int code=e.getKeyCode();
+                  switch(code){
 
-			while(win != true){
-				/* 커서 움직이기 하지만 개선이 되어야 한다!!! */
-				Scanner s = new Scanner(System.in);
+                     case 38:
+                     if(User.GetY() > 10) User.SetLocation(User.GetX(), User.GetY()-40);
+                     frame.repaint();
+                     win = Board.win;
+                     break;
 
-				keycode = s.next().charAt(0);
+                     case 40:
+                     if(User.GetY() < 730) User.SetLocation(User.GetX(), User.GetY()+40);
+                     frame.repaint();
+                     win = Board.win;
+                     break;
 
-				switch(keycode){
+                     case 37:
+                     if(User.GetX() > 10) User.SetLocation(User.GetX()-40, User.GetY());
+                     frame.repaint();
+                     win = Board.win;
+                     break;
 
-					case 'w':
-					if(User.GetY() > 10) User.SetLocation(User.GetX(), User.GetY()-40);
-					break;
+                     case 39:
+                     if(User.GetX() < 730) User.SetLocation(User.GetX()+40, User.GetY());
+                     frame.repaint();
+                     win = Board.win;
+                     break;
 
-					case 's':
+                     case 32: 
+                     setboard[k].SetLocation(User.GetX(),User.GetY()); // 놓은 돌의 위치를 setboard 배열에 계속 저장해준다.
+                     for(int i=0;i<361;i++)
+                        if((setboard[k].GetX()==board[i].GetX())&&
+                           (setboard[k].GetY()==board[i].GetY()))
 
-					if(User.GetY() < 730) User.SetLocation(User.GetX(), User.GetY()+40);
-					break;
+                           if(board[i].Getis() == false)
+                           {
+                              board[i].Setis(true);
+                              
 
-					case 'a':
+                              if(turnW > 0)
+                              {
+                                 board[i].BW = W;
+                                 total_Score.setTotal(board[i]);
+                                 turnW-- ;
+                                 if(turnW == 0) turnB = 2;
+                              }
+                              else if(turnB > 0) 
+                              {
+                                 board[i].BW = B;
+                                 total_Score.setTotal(board[i]);
+                                 turnB-- ;
+                                 if(turnB == 0) turnW = 2;
+                              }
 
-					if(User.GetX() > 10) User.SetLocation(User.GetX()-40, User.GetY());
-					break;
-
-					case 'd':
-
-					if(User.GetX() < 730) User.SetLocation(User.GetX()+40, User.GetY());
-					break;
-
-					case 'x': 
-
-					setboard[k].SetLocation(User.GetX(),User.GetY()); // 놓은 돌의 위치를 setboard 배열에 계속 저장해준다.
-
-					for(i=0;i<361;i++)
-						if((setboard[k].GetX()==board[i].GetX())&&
-						   (setboard[k].GetY()==board[i].GetY()))
-
-							if(board[i].Getis() == false)
-							{
-								board[i].Setis(true);
-								
-
-								if(turnW > 0)
-								{
-									board[i].BW = W;
-									total_Score.setTotal(board[i]);
-									turnW-- ;
-									if(turnW == 0) turnB = 2;
-								}
-								else if(turnB > 0) 
-								{
-									board[i].BW = B;
-									total_Score.setTotal(board[i]);
-									turnB-- ;
-									if(turnB == 0) turnW = 2;
-								}
-
-								k++;
-							}
-
-
-					total_Score.display();
-					
-					break ;
-				}
-				
-				frame.repaint();
-				win = Board.win;
-
-			}
+                              k++;
+                           }
 
 
-			return ;
-	}
+                     total_Score.display();
+                     frame.repaint();
+                     win = Board.win;
+                     break ;
+                     default:
+                     break;
+               }
+            }
+            public void keyReleased(java.awt.event.KeyEvent e) {
+            }
+
+
+            public void keyTyped(java.awt.event.KeyEvent e) {   
+            }
+         }   
+         frame.setVisible(true);
+         frame.addKeyListener(new Key());
+         return ;
+   }
 }
