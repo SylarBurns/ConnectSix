9525169da464194c0af8f2415038142c10337ff5
SylarBurns <41365906+SylarBurns@users.noreply.github.com>
2018-11-05 21:29:48 +0900

    Add files via upload
    GUI 개선

--- Board.java
+++ Board.java
@@ -25,7 +25,7 @@ class Board extends JPanel {
 		g.drawString("Right : d\n", 800, 70) ;
 		g.drawString("Stone Down : space\n", 800, 85) ;
 		g.drawString((Gomoku.total_Score.get_first_where()%19+1)+", "+((Gomoku.total_Score.get_first_where())/19+1), 800, 100) ;
-
+		g.drawString("W: "+Gomoku.turnW+"/nB: "+Gomoku.turnB, 800, 115);
 		for(int i=25; i<750; i=i+40){
 			
 		g.drawLine(25, i, 745, i);
@@ -107,8 +107,8 @@ class Board extends JPanel {
 		{
 			g.setColor(new Color(0,0,0));
 
-			if(winner == B)		    	g.drawString("Black win", 800, 100);
-			else if(winner == W)		g.drawString("White win", 800, 100);
+			if(winner == B)		    	g.drawString("Black win", 800, 130);
+			else if(winner == W)		g.drawString("White win", 800, 130);
 			
 		}
 	
@@ -116,7 +116,7 @@ class Board extends JPanel {
 		g.fillRect(Gomoku.User.GetX()+2, Gomoku.User.GetY()+2,26,26);
 		g.setColor(new Color(0,0,255));
 		g.fillRect((Gomoku.total_Score.get_first_where()%19+1)*40-25, (Gomoku.total_Score.get_first_where()/19+1)*40-25, 20, 20);
-		
+
 		if(Gomoku.k==361)  g.drawString("Draw", 800, 100);
 	}
 
